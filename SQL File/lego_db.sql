981aac5305079d37fe9cb728a853962adb282b24
TB. Maulana Aghni <58358913+maulanaaghnii@users.noreply.github.com>
2021-07-19 21:50:08 +0700

    Add files via upload

--- SQL File/lego_db.sql
+++ SQL File/lego_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2021 at 08:43 AM
+-- Generation Time: Jul 19, 2021 at 04:48 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
